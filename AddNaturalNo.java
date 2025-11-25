d4d24c19dcdf96d9bd58a041dabaf357f1bf29ae
rajsachin7061 <rajsachin7061@gmail.com>
2025-11-26 00:25:31 +0530

    All Natural Sum

---
+++ AddNaturalNo.java
@@ -0,0 +1,13 @@
+public class AddNaturalNo {
+    public static void main(String []args) {
+        int sum = 0;
+        int n = 5;
+
+        for (int i=0; i<=n; i++) {
+            sum = sum+i;
+
+        }
+        System.out.println("Sum of natural no = " +sum);
+    }
+    
+}
